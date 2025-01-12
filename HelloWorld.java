ecb00c67e19616cae4491a2feb3db35976203243
heyrips <100979172+heyrips@users.noreply.github.com>
2025-01-11 23:30:42 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 //my java program
 class HelloWorld {
-   public static void main (String[]args){
-   System.out.println("Java, Sample code!");
-   }
+    public static void main(String[] args) {
+        System.out.println("Java, Sample code!");
+    }
 }
